b97ff07cc9901e79abd71de89b91cc7e4f7e20d4
rkk1995 <theravindu@gmail.com>
2022-07-23 20:20:17 -0400

    Tests for natural sorting

---
+++ mkdocs_awesome_pages_plugin/tests/e2e/test_order_and_sort.py
@@ -0,0 +1,254 @@
+from .base import E2ETestCase
+
+class TestOrderAndSort(E2ETestCase):
+    def test_asc(self):
+        navigation = self.mkdocs(
+            self.config,
+            [
+                "1.md",
+                "3.md",
+                ("2", ["1.md", "2.md", self.pagesFile(order="asc")]),
+                self.pagesFile(order="asc"),
+            ],
+        )
+
+        self.assertEqual(
+            navigation,
+            [("1", "/1"), ("2", [("1", "/2/1"), ("2", "/2/2")]), ("3", "/3")],
+        )
+
+    def test_asc_natural(self):
+        navigation = self.mkdocs(
+            self.config,
+            [
+                "100.md",
+                "3.md",
+                ("20", ["100.md", "20.md", self.pagesFile(order="asc", sort_type="natural")]),
+                self.pagesFile(order="asc", sort_type="natural"),
+            ],
+        )
+
+        self.assertEqual(
+            navigation,
+            [("3", "/3"), ("20", [("20", "/20/20"), ("100", "/20/100")]), ("100", "/100")],
+        )
+
+    def test_desc(self):
+        navigation = self.mkdocs(
+            self.config,
+            [
+                "1.md",
+                "3.md",
+                ("2", ["1.md", "2.md", self.pagesFile(order="desc")]),
+                self.pagesFile(order="desc"),
+            ],
+        )
+
+        self.assertEqual(
+            navigation,
+            [("3", "/3"), ("2", [("2", "/2/2"), ("1", "/2/1")]), ("1", "/1")],
+        )
+
+    def test_desc_natural(self):
+        navigation = self.mkdocs(
+            self.config,
+            [
+                "100.md",
+                "3.md",
+                ("20", ["100.md", "20.md", self.pagesFile(order="desc", sort_type="natural")]),
+                self.pagesFile(order="desc", sort_type="natural"),
+            ],
+        )
+
+        self.assertEqual(
+            navigation,
+            [("100", "/100"), ("20", [("100", "/20/100"), ("20", "/20/20")]), ("3", "/3")],
+        )
+
+    def test_nav_asc(self):
+        navigation = self.mkdocs(
+            self.config,
+            [
+                "1.md",
+                "3.md",
+                (
+                    "2",
+                    ["1.md", "2.md", self.pagesFile(order="asc", nav=["2.md", "1.md"])],
+                ),
+                self.pagesFile(order="asc", nav=["2", "1.md", "3.md"]),
+            ],
+        )
+
+        self.assertEqual(
+            navigation,
+            [("2", [("2", "/2/2"), ("1", "/2/1")]), ("1", "/1"), ("3", "/3")],
+        )
+
+    def test_nav_asc_natural(self):
+        navigation = self.mkdocs(
+            self.config,
+            [
+                "100.md",
+                "3.md",
+                (
+                    "20",
+                    ["1.md", "2.md", self.pagesFile(order="asc", sort_type="natural", nav=["2.md", "1.md"])],
+                ),
+                self.pagesFile(order="asc", sort_type="natural", nav=["20", "100.md", "3.md"]),
+            ],
+        )
+
+        self.assertEqual(
+            navigation,
+            [("20", [("2", "/20/2"), ("1", "/20/1")]), ("100", "/100"), ("3", "/3")],
+        )
+
+    def test_nav_desc(self):
+        navigation = self.mkdocs(
+            self.config,
+            [
+                "1.md",
+                "3.md",
+                (
+                    "2",
+                    [
+                        "1.md",
+                        "2.md",
+                        self.pagesFile(order="desc", nav=["2.md", "1.md"]),
+                    ],
+                ),
+                self.pagesFile(order="desc", nav=["2", "1.md", "3.md"]),
+            ],
+        )
+
+        self.assertEqual(
+            navigation,
+            [("2", [("2", "/2/2"), ("1", "/2/1")]), ("1", "/1"), ("3", "/3")],
+        )
+
+    def test_nav_desc_natural(self):
+        navigation = self.mkdocs(
+            self.config,
+            [
+                "100.md",
+                "3.md",
+                (
+                    "20",
+                    ["1.md", "2.md", self.pagesFile(order="desc", sort_type="natural", nav=["2.md", "1.md"])],
+                ),
+                self.pagesFile(order="desc", sort_type="natural", nav=["20", "100.md", "3.md"]),
+            ],
+        )
+
+        self.assertEqual(
+            navigation,
+            [("20", [("2", "/20/2"), ("1", "/20/1")]), ("100", "/100"), ("3", "/3")],
+        )
+
+    def test_nav_rest_asc(self):
+        navigation = self.mkdocs(
+            self.config,
+            [
+                "1.md",
+                "3.md",
+                (
+                    "2",
+                    [
+                        "1.md",
+                        "2.md",
+                        "3.md",
+                        self.pagesFile(order="asc", nav=["3.md", "..."]),
+                    ],
+                ),
+                self.pagesFile(order="asc", nav=["3.md", "..."]),
+            ],
+        )
+
+        self.assertEqual(
+            navigation,
+            [
+                ("3", "/3"),
+                ("1", "/1"),
+                ("2", [("3", "/2/3"), ("1", "/2/1"), ("2", "/2/2")]),
+            ],
+        )
+
+    def test_nav_rest_asc_natural(self):
+        navigation = self.mkdocs(
+            self.config,
+            [
+                "100.md",
+                "3.md",
+                (
+                    "20",
+                    [
+                        "100.md",
+                        "20.md",
+                        "3.md",
+                        self.pagesFile(order="asc", sort_type="natural", nav=["3.md", "..."]),
+                    ],
+                ),
+                self.pagesFile(order="asc", sort_type="natural", nav=["3.md", "..."]),
+            ],
+        )
+
+        self.assertEqual(
+            navigation,
+            [("3", "/3"), ("20", [("3", "/20/3"), ("20", "/20/20"), ("100", "/20/100")]), ("100", "/100")],
+        )
+
+    def test_nav_rest_desc(self):
+        navigation = self.mkdocs(
+            self.config,
+            [
+                "1.md",
+                "3.md",
+                (
+                    "2",
+                    [
+                        "1.md",
+                        "2.md",
+                        "3.md",
+                        self.pagesFile(order="desc", nav=["1.md", "..."]),
+                    ],
+                ),
+                self.pagesFile(order="desc", nav=["1.md", "..."]),
+            ],
+        )
+
+        self.assertEqual(
+            navigation,
+            [
+                ("1", "/1"),
+                ("3", "/3"),
+                ("2", [("1", "/2/1"), ("3", "/2/3"), ("2", "/2/2")]),
+            ],
+        )
+
+    def test_nav_rest_desc_natural(self):
+        navigation = self.mkdocs(
+            self.config,
+            [
+                "100.md",
+                "3.md",
+                (
+                    "20",
+                    [
+                        "100.md",
+                        "20.md",
+                        "3.md",
+                        self.pagesFile(order="desc", sort_type="natural", nav=["3.md", "..."]),
+                    ],
+                ),
+                self.pagesFile(order="desc", sort_type="natural", nav=["3.md", "..."]),
+            ],
+        )
+
+        self.assertEqual(
+            navigation,
+            [
+                ("3", "/3"),
+                ("100", "/100"),
+                ("20", [("3", "/20/3"), ("100", "/20/100"), ("20", "/20/20")]),
+            ],
+        )
---
+++ mkdocs_awesome_pages_plugin/tests/navigation/test_order_and_sort.py
@@ -0,0 +1,290 @@
+from .base import NavigationTestCase
+from ...meta import Meta, MetaNavItem, MetaNavRestItem
+
+class TestOrderAndSort(NavigationTestCase):
+    def test_asc(self):
+        navigation = self.createAwesomeNavigation(
+            [self.page("1"), self.page("2"), self.page("3"), Meta(order=Meta.ORDER_ASC)]
+        )
+
+        self.assertNavigationEqual(navigation.items, [self.page("1"), self.page("2"), self.page("3")])
+        self.assertValidNavigation(navigation.to_mkdocs())
+
+    def test_asc_natural(self):
+        navigation = self.createAwesomeNavigation(
+            [
+                self.page("100"),
+                self.page("20"),
+                self.page("3"),
+                Meta(order=Meta.ORDER_ASC, sort_type=Meta.SORT_NATURAL),
+            ]
+        )
+
+        self.assertNavigationEqual(navigation.items, [self.page("3"), self.page("20"), self.page("100")])
+        self.assertValidNavigation(navigation.to_mkdocs())
+
+    def test_asc_natural_with_dot_suffix(self):
+        navigation = self.createAwesomeNavigation(
+            [
+                self.page("100."),
+                self.page("20."),
+                self.page("3."),
+                Meta(order=Meta.ORDER_ASC, sort_type=Meta.SORT_NATURAL),
+            ]
+        )
+
+        self.assertNavigationEqual(navigation.items, [self.page("3."), self.page("20."), self.page("100.")])
+        self.assertValidNavigation(navigation.to_mkdocs())
+
+    def test_desc(self):
+        navigation = self.createAwesomeNavigation(
+            [
+                self.page("1"),
+                self.page("2"),
+                self.page("3"),
+                Meta(order=Meta.ORDER_DESC),
+            ]
+        )
+
+        self.assertNavigationEqual(navigation.items, [self.page("3"), self.page("2"), self.page("1")])
+        self.assertValidNavigation(navigation.to_mkdocs())
+
+    def test_desc_natural(self):
+        navigation = self.createAwesomeNavigation(
+            [
+                self.page("100"),
+                self.page("20"),
+                self.page("3"),
+                Meta(order=Meta.ORDER_DESC, sort_type=Meta.SORT_NATURAL),
+            ]
+        )
+
+        self.assertNavigationEqual(navigation.items, [self.page("100"), self.page("20"), self.page("3")])
+        self.assertValidNavigation(navigation.to_mkdocs())
+
+    def test_section_asc(self):
+        navigation = self.createAwesomeNavigation(
+            [
+                self.page("1"),
+                self.page("3"),
+                self.section("2", [self.page("a", "2/a.md")], "2"),
+                Meta(order=Meta.ORDER_ASC),
+            ]
+        )
+
+        self.assertNavigationEqual(
+            navigation.items,
+            [
+                self.page("1"),
+                self.section("2", [self.page("a", "2/a.md")], "2"),
+                self.page("3"),
+            ],
+        )
+        self.assertValidNavigation(navigation.to_mkdocs())
+
+    def test_section_asc_natural(self):
+        navigation = self.createAwesomeNavigation(
+            [
+                self.page("100"),
+                self.page("3"),
+                self.section("20", [self.page("a", "2/a.md")], "20"),
+                Meta(order=Meta.ORDER_ASC, sort_type=Meta.SORT_NATURAL),
+            ]
+        )
+        self.assertNavigationEqual(
+            navigation.items,
+            [
+                self.page("3"),
+                self.section("20", [self.page("a", "2/a.md")], "20"),
+                self.page("100"),
+            ],
+        )
+        self.assertValidNavigation(navigation.to_mkdocs())
+
+    def test_section_desc(self):
+        navigation = self.createAwesomeNavigation(
+            [
+                self.page("1"),
+                self.page("3"),
+                self.section("2", [self.page("a", "2/a.md")], "2"),
+                Meta(order=Meta.ORDER_DESC),
+            ]
+        )
+
+        self.assertNavigationEqual(
+            navigation.items,
+            [
+                self.page("3"),
+                self.section("2", [self.page("a", "2/a.md")], "2"),
+                self.page("1"),
+            ],
+        )
+        self.assertValidNavigation(navigation.to_mkdocs())
+
+    def test_section_desc_natural(self):
+        navigation = self.createAwesomeNavigation(
+            [
+                self.page("100"),
+                self.page("3"),
+                self.section("20", [self.page("a", "2/a.md")], "20"),
+                Meta(order=Meta.ORDER_DESC, sort_type=Meta.SORT_NATURAL),
+            ]
+        )
+
+        self.assertNavigationEqual(
+            navigation.items,
+            [
+                self.page("100"),
+                self.section("20", [self.page("a", "2/a.md")], "20"),
+                self.page("3"),
+            ],
+        )
+        self.assertValidNavigation(navigation.to_mkdocs())
+
+    def test_nav_asc(self):
+        navigation = self.createAwesomeNavigation(
+            [
+                self.page("1"),
+                self.page("2"),
+                self.page("3"),
+                Meta(
+                    order=Meta.ORDER_ASC,
+                    nav=[MetaNavItem("2.md"), MetaNavItem("3.md"), MetaNavItem("1.md")],
+                ),
+            ]
+        )
+
+        self.assertNavigationEqual(navigation.items, [self.page("2"), self.page("3"), self.page("1")])
+        self.assertValidNavigation(navigation.to_mkdocs())
+
+    def test_nav_asc_natural(self):
+        navigation = self.createAwesomeNavigation(
+            [
+                self.page("100"),
+                self.page("20"),
+                self.page("3"),
+                Meta(
+                    order=Meta.ORDER_ASC,
+                    sort_type=Meta.SORT_NATURAL,
+                    nav=[MetaNavItem("20.md"), MetaNavItem("3.md"), MetaNavItem("100.md")],
+                ),
+            ]
+        )
+
+        self.assertNavigationEqual(navigation.items, [self.page("20"), self.page("3"), self.page("100")])
+        self.assertValidNavigation(navigation.to_mkdocs())
+
+    def test_nav_desc(self):
+        navigation = self.createAwesomeNavigation(
+            [
+                self.page("1"),
+                self.page("2"),
+                self.page("3"),
+                Meta(
+                    order=Meta.ORDER_DESC,
+                    nav=[MetaNavItem("2.md"), MetaNavItem("3.md"), MetaNavItem("1.md")],
+                ),
+            ]
+        )
+
+        self.assertNavigationEqual(navigation.items, [self.page("2"), self.page("3"), self.page("1")])
+        self.assertValidNavigation(navigation.to_mkdocs())
+
+    def test_nav_desc_natural(self):
+        navigation = self.createAwesomeNavigation(
+            [
+                self.page("100"),
+                self.page("20"),
+                self.page("3"),
+                Meta(
+                    order=Meta.ORDER_DESC,
+                    sort_type=Meta.SORT_NATURAL,
+                    nav=[MetaNavItem("20.md"), MetaNavItem("3.md"), MetaNavItem("100.md")],
+                ),
+            ]
+        )
+
+        self.assertNavigationEqual(navigation.items, [self.page("20"), self.page("3"), self.page("100")])
+        self.assertValidNavigation(navigation.to_mkdocs())
+
+    def test_nav_rest_asc(self):
+        navigation = self.createAwesomeNavigation(
+            [
+                self.page("1"),
+                self.page("2"),
+                self.page("3"),
+                self.page("4"),
+                Meta(
+                    order=Meta.ORDER_ASC,
+                    nav=[MetaNavItem("4.md"), MetaNavRestItem("...")],
+                ),
+            ]
+        )
+
+        self.assertNavigationEqual(
+            navigation.items,
+            [self.page("4"), self.page("1"), self.page("2"), self.page("3")],
+        )
+        self.assertValidNavigation(navigation.to_mkdocs())
+
+    def test_nav_rest_asc_natural(self):
+        navigation = self.createAwesomeNavigation(
+            [
+                self.page("1000"),
+                self.page("200"),
+                self.page("30"),
+                self.page("4"),
+                Meta(
+                    order=Meta.ORDER_ASC,
+                    sort_type=Meta.SORT_NATURAL,
+                    nav=[MetaNavItem("30.md"), MetaNavRestItem("...")],
+                ),
+            ]
+        )
+
+        self.assertNavigationEqual(
+            navigation.items,
+            [self.page("30"), self.page("4"), self.page("200"), self.page("1000")],
+        )
+        self.assertValidNavigation(navigation.to_mkdocs())
+
+    def test_nav_rest_desc(self):
+        navigation = self.createAwesomeNavigation(
+            [
+                self.page("1"),
+                self.page("2"),
+                self.page("3"),
+                self.page("4"),
+                Meta(
+                    order=Meta.ORDER_DESC,
+                    nav=[MetaNavItem("1.md"), MetaNavRestItem("...")],
+                ),
+            ]
+        )
+
+        self.assertNavigationEqual(
+            navigation.items,
+            [self.page("1"), self.page("4"), self.page("3"), self.page("2")],
+        )
+        self.assertValidNavigation(navigation.to_mkdocs())
+
+    def test_nav_rest_desc_natural(self):
+        navigation = self.createAwesomeNavigation(
+            [
+                self.page("1000"),
+                self.page("200"),
+                self.page("30"),
+                self.page("4"),
+                Meta(
+                    order=Meta.ORDER_DESC,
+                    sort_type=Meta.SORT_NATURAL,
+                    nav=[MetaNavItem("30.md"), MetaNavRestItem("...")],
+                ),
+            ]
+        )
+
+        self.assertNavigationEqual(
+            navigation.items,
+            [self.page("30"), self.page("1000"), self.page("200"), self.page("4")],
+        )
+        self.assertValidNavigation(navigation.to_mkdocs())
