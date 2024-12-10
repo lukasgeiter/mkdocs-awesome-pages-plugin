94bc80c12458ee559ceeb2e7e391a459d6c54aa1
lcvgit <121000412+lcvgit@users.noreply.github.com>
2024-12-10 12:00:01 -0600

    Update options.py

--- mkdocs_awesome_pages_plugin/options.py
+++ mkdocs_awesome_pages_plugin/options.py
@@ -8,6 +8,7 @@ class Options:
         order: str = None,
         sort_type: str = None,
         order_by: str = None,
+        ignore_case: bool = None,
     ):
         self.filename = filename
         self.collapse_single_pages = collapse_single_pages
@@ -15,3 +16,4 @@ class Options:
         self.order = order
         self.sort_type = sort_type
         self.order_by = order_by
+        self.ignore_case = ignore_case
